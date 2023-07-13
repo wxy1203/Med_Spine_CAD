0c5766373e7fce02f87e4667bb21fdf68573e486
Wang Xinyi <e0817822@u.nus.edu>
2023-07-13 09:23:44 +0800

    Update

---
+++ Spine_Cmn_Math/_SPN_Cmn_Math__inLib.h
@@ -0,0 +1,10 @@
+#ifndef _SPN_Cmn_Math__inLib__INCLUDED_
+#define	_SPN_Cmn_Math__inLib__INCLUDED_
+
+#pragma once
+
+#define SPN_Cmn_Math_LIB
+
+#include "_SPN_Cmn_Math__Include.h"
+
+#endif //  // !defined(_SPN_Cmn_Math__inLib__INCLUDED_
